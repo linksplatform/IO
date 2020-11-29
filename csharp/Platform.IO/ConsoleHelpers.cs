d5e9f8f847f8765eabbcf9ca8737abd633c18768
FreePhoenix <freephoenix888@gmail.com>
2020-11-29 12:24:02 +0600

    XML Documentation: GetOrReadArguments Public function Debug

--- csharp/Platform.IO/ConsoleHelpers.cs
+++ csharp/Platform.IO/ConsoleHelpers.cs
@@ -10,14 +10,16 @@ namespace Platform.IO
 {
 	public static class ConsoleHelpers
 	{
+
 		/// <summary>
-		/// <para>Displays in console appealing to press any key for continue. Checks if a user pressed any key.</para>
-		/// <para>Выводит в консоли призыв нажать любую клавишу для продолжения. Проверяет нажал ли пользователь любую кнопку.</para>
+		/// <para>Displays in console appealing to press any key for continue</para>
+		/// <para>Выводит в консоли призыв нажать любую клавишу для продолжения</para>
 		/// </summary>
 		/// <returns>
 		/// <para>Returns true if a user pressed any key.</para>
-		/// <para>Возвращает истину если пользователь нажал любую клавишу.</para>
-		/// </returns>	
+		/// <para>Возвращает истину если пользователь нажал любую клавишу</para>
+		/// </returns>
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void PressAnyKeyToContinue()
 		{
@@ -31,10 +33,23 @@ namespace Platform.IO
 		/// </summary>
 		/// <param name="index">Index integer</param>
 		/// <param name="args"></param>
-		/// <returns></returns>
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static string GetOrReadArgument(int index, params string[] args) => GetOrReadArgument(index, $"{index + 1} argument", args);
+
+		/// <summary>
+		/// <para></para>
+		/// </summary>
+		/// <param name="index">
+        /// <para>Item number in the array</para>
+        /// <para>Номер элемента в массиве</para></param>
+		/// <param name="readMessage">
+        /// <para>Text to output in the console</para>
+        /// <para>Текст для вывода в консоль</para></param>
+		/// <param name="args">
+        /// <para>Arguments array with string type</para>
+        /// <para>Массив аргументов типа строка</para></param>
+		/// <returns><para>Returns text passed to console with double quotes and whitespace around removed</para><para>Возвращает текст переданный в консоль с удалёнными пробелами и двойными ковычками вокруг текста</para></returns>
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static string GetOrReadArgument(int index, string readMessage, params string[] args)
@@ -54,8 +69,22 @@ namespace Platform.IO
 			}
 		}
 
+		/// <summary>
+		/// <para>Output a input string to the console</para>
+		/// </summary>
+		/// <param name="string">
+        /// <para>Text passed to the console</para>
+        /// <para>Текст переданный в консоль</para>
+        /// </param>
+
 		[Conditional("DEBUG")]
 		public static void Debug(string @string) => Console.WriteLine(@string);
+
+		/// <summary>
+        ///
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
 
 		[Conditional("DEBUG")]
 		public static void Debug(string format, params object[] args) => Console.WriteLine(format, args);
