5c493d0b1b99def2ae341cb0bc16aec84b35d011
FreePhoenix <freephoenix888@gmail.com>
2021-03-05 19:32:21 +0600

    ReadAll method - docs

--- csharp/Platform.IO/StreamExtensions.cs
+++ csharp/Platform.IO/StreamExtensions.cs
@@ -9,19 +9,19 @@ namespace Platform.IO
     public static class StreamExtensions
     {
         /// <summary>
-        /// <para>Writes a sequence of bytes representing <paramref name = "value"/> to the stream <paramref name = "stream"/>.</para>
-        /// <para>Записывает последовательность байт представляющую <paramref name="value"/> в  поток <paramref name="stream"/>.</para>
+        /// <para>Writes a sequence of bytes representing <paramref name="value"/> to the stream <paramref name="stream"/> and moves the current position within the <paramref name="stream"/> by the number of written bytes.</para>
+        /// <para>Записывает последовательность байт представляющую <paramref name="value"/> в  поток <paramref name="stream"/> и перемещает текущую позицию в нем вперед на число записанных байт.</para>
         /// </summary>
         /// <typeparam name="T">
-        /// <para>Struct.</para>
-        /// <para>Структура.</para>
+        /// <para>Type being structure.</para>
+        /// <para>Тип являющийся структурой.</para>
         /// </typeparam>
         /// <param name="stream">
-        /// <para>An abstract class that provides an overview of a sequence of bytes.</para>
-        /// <para>Абстрактный класс предоставляющий универсальное представление последовательности байтов.</para>
+        /// <para>A stream to record.</para>
+        /// <para>Поток, в который осуществляется запись.</para>
         /// </param>
         /// <param name="value">
-        /// <para>Struct whose value pass to the stream <paramref name="stream"/>.</para>
+        /// <para>The struct value to be written to the <paramref name="stream"/>..</para>
         /// <para>Структура, значение которой записывается в поток <paramref name="stream"/>.</para>
         /// </param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -33,18 +33,21 @@ namespace Platform.IO
         }
 
         /// <summary>
-        /// <para>Reads a sequence of bytes representing.</para>
-        /// <para></para>
+        /// <para>Reads a sequence of bytes representing the structure and moves the current position within the <paramref name="stream"/> by the number of bytes read.</para>
+        /// <para>Прочитывает последовательность байт представляющих структуру и перемещает текущую позицию в <paramref name="stream"/> вперед на число прочитанных байт..</para>
         /// </summary>
         /// <typeparam name="T">
-        /// <para>Struct.</para>
-        /// <para>Структура.</para>
+        /// <para>Type being structure.</para>
+        /// <para>Тип являющийся структурой.</para>
         /// </typeparam>
         /// <param name="stream">
-        /// <para>An abstract class that provides an overview of a sequence of bytes.</para>
-        /// <para>Абстрактный класс предоставляющий универсальное представление последовательности байтов.</para>
+        /// <para>A stream containing the structure.</para>
+        /// <para>Поток, содержащий структуру.</para>
         /// </param>
-        /// <returns></returns>
+        /// <returns>
+        /// <para>The structure, if all bytes from stream have been read, otherwise the structure with default fields values.</para>
+        /// <para>Структура, если все байты из потока были прочитаны, иначе структура с значениями полей по умолчанию.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T ReadOrDefault<T>(this Stream stream)
             where T : struct
@@ -54,6 +57,12 @@ namespace Platform.IO
             return stream.Read(buffer, 0, size) == size ? buffer.ToStructure<T>() : default;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="stream"></param>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T[] ReadAll<T>(this Stream stream)
             where T : struct
