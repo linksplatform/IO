92698f4309f1a65bfb213a3f8996987d01d33a45
FreePhoenix <freephoenix888@gmail.com>
2021-06-24 23:25:14 +0600

    Add TemporaryFileExsitsTest()

---
+++ csharp/Platform.IO.Tests/TemporaryFileTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Platform.IO;
+using System.IO;
+using System.Diagnostics;
+
+namespace Platform.IO.Tests
+{
+    public class TemporaryFileTests
+    {
+        [Fact]
+        public void TemporaryFileExistsTest()
+        {
+            using Process process = new Process();
+            process.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+            process.WaitForExit();
+        }
+
+    }
+}
