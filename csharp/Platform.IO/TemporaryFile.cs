587c89685a3a2b60801b77368809f1e41d49c9ac
FreePhoenix <freephoenix888@gmail.com>
2021-06-24 23:27:31 +0600

    The beginning

---
+++ csharp/Platform.IO/TemporaryFile.cs
@@ -0,0 +1,28 @@
+﻿using Platform.Disposables;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.IO
+{
+    public class TemporaryFile : DisposableBase
+    {
+        public readonly string Filename;
+
+        public TemporaryFile()
+        {
+            Filename = TemporaryFiles.UseNew();
+        }
+
+        protected override void Dispose(bool manual, bool wasDisposed)
+        {
+            if (!wasDisposed)
+            {
+                File.Delete(Filename);
+            }
+        }
+    }
+}
