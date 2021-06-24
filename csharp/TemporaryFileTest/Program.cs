83da4679a8e381713b88ab1001d42158e61e0bac
FreePhoenix <freephoenix888@gmail.com>
2021-06-24 23:28:03 +0600

    Add console app for tests

---
+++ csharp/TemporaryFileTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Platform.IO;
+using System.IO;
+
+namespace TemporaryFileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemporaryFile tempFile = new TemporaryFile();
+            Console.WriteLine($"File exists? {File.Exists(tempFile.Filename)}");
+        }
+    }
+}
