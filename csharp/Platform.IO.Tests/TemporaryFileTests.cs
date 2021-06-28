c707ffc6b243bd7f2dfdf7f1f2234a408cc366f6
FreePhoenix <freephoenix888@gmail.com>
2021-06-28 11:35:38 +0600

    Add XUnit output

--- csharp/Platform.IO.Tests/TemporaryFileTests.cs
+++ csharp/Platform.IO.Tests/TemporaryFileTests.cs
@@ -7,11 +7,17 @@ using System.Threading.Tasks;
 using Platform.IO;
 using System.IO;
 using System.Diagnostics;
+using Xunit.Abstractions;
 
 namespace Platform.IO.Tests
 {
     public class TemporaryFileTests
     {
+        private readonly ITestOutputHelper output;
+        public TemporaryFileTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
         [Fact]
         public void TemporaryFileExistsTest()
         {
