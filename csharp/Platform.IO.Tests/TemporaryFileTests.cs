af7e9006d41cb006115fd4f38aa5eb6fd64f8370
FreePhoenix <freephoenix888@gmail.com>
2021-06-29 23:48:25 +0600

    Output bin files

--- csharp/Platform.IO.Tests/TemporaryFileTests.cs
+++ csharp/Platform.IO.Tests/TemporaryFileTests.cs
@@ -27,7 +27,7 @@ namespace Platform.IO.Tests
             bool isLinux = System.Runtime.InteropServices.RuntimeInformation
                                                .IsOSPlatform(OSPlatform.Linux);
             using Process process = new Process();
-            process.StartInfo.FileName = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TemporaryFileTest", "bin", "Debug", "net5"));
+            process.StartInfo.FileName = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TemporaryFileTest", "bin"));
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
             //process.Start();
