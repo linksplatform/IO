bdf9c4e68e7d8bf907e8e4d178e5bc524a67c91e
FreePhoenix <freephoenix888@gmail.com>
2021-06-29 23:46:30 +0600

    Output build files

--- csharp/Platform.IO.Tests/TemporaryFileTests.cs
+++ csharp/Platform.IO.Tests/TemporaryFileTests.cs
@@ -27,13 +27,13 @@ namespace Platform.IO.Tests
             bool isLinux = System.Runtime.InteropServices.RuntimeInformation
                                                .IsOSPlatform(OSPlatform.Linux);
             using Process process = new Process();
-            process.StartInfo.FileName = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TemporaryFileTest"));
+            process.StartInfo.FileName = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TemporaryFileTest", "bin", "Debug", "net5"));
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
             //process.Start();
             output.WriteLine(process.StartInfo.FileName);
             output.WriteLine($"{File.Exists(process.StartInfo.FileName)}");
-            foreach (var item in Directory.GetDirectories(process.StartInfo.FileName))
+            foreach (var item in Directory.GetFiles(process.StartInfo.FileName))
             {
                 output.WriteLine(item);
             }
