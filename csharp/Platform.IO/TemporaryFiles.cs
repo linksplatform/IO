6ac9bbea416f6be5e6128aecabbbe3414c1c5635
FreePhoenix <freephoenix888@gmail.com>
2021-07-02 16:21:03 +0600

    Remove empty lines

--- csharp/Platform.IO/TemporaryFiles.cs
+++ csharp/Platform.IO/TemporaryFiles.cs
@@ -67,7 +67,6 @@ namespace Platform.IO
                         }
                     }
                 }
-
                 using (File.Open(usedFilesListFilename, FileMode.Truncate)) { }
             }
         }
