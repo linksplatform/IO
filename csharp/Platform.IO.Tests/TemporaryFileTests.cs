e694bc60867292d13104dccd95024c4bc8849151
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-15 15:48:25 +0600

    Comment TemporaryFileTest

--- csharp/Platform.IO.Tests/TemporaryFileTests.cs
+++ csharp/Platform.IO.Tests/TemporaryFileTests.cs
@@ -6,24 +6,24 @@ namespace Platform.IO.Tests
 {
     public class TemporaryFileTests
     {
-        [Fact(Skip = "")]
-        public void TemporaryFileTest()
-        {
-            var startInfo = new ProcessStartInfo
-            {
-                WorkingDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "Platform.IO.Tests.TemporaryFileTest")),
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                FileName = "dotnet",
-                Arguments = "run --project Platform.IO.Tests.TemporaryFileTest.csproj"
-            };
-            using Process process = new(){StartInfo = startInfo};
-            process.Start();
-            var path = process.StandardOutput.ReadLine();
-            Assert.True(File.Exists(path));
-            process.WaitForExit();
-            Assert.False(File.Exists(path));
-        }
+        // [Fact(Skip = "")]
+        // public void TemporaryFileTest()
+        // {
+        //     var startInfo = new ProcessStartInfo
+        //     {
+        //         WorkingDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "Platform.IO.Tests.TemporaryFileTest")),
+        //         UseShellExecute = false,
+        //         RedirectStandardOutput = true,
+        //         FileName = "dotnet",
+        //         Arguments = "run --project Platform.IO.Tests.TemporaryFileTest.csproj"
+        //     };
+        //     using Process process = new(){StartInfo = startInfo};
+        //     process.Start();
+        //     var path = process.StandardOutput.ReadLine();
+        //     Assert.True(File.Exists(path));
+        //     process.WaitForExit();
+        //     Assert.False(File.Exists(path));
+        // }
 
         [Fact]
         public void TemporaryFileTestWithoutConsoleApp()
